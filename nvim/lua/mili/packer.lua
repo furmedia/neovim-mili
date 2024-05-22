5adf4b155481bdc3524317ed46bf70ce469dd81e
furmedia <50455763+furmedia@users.noreply.github.com>
2024-05-22 09:34:39 +0200

    Update packer.lua

--- nvim/lua/mili/packer.lua
+++ nvim/lua/mili/packer.lua
@@ -1,32 +1,26 @@
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
-	use({
-		'rose-pine/neovim',
-		as = 'rose-pine',
-		config = function()
-			vim.cmd('colorscheme rose-pine')
-		end})
-
-use({
-    "stevearc/conform.nvim",
+-- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+    use({ 'rose-pine/neovim',
+	as = 'rose-pine',
+	config = function()
+		vim.cmd('colorscheme rose-pine')
+		vim.cmd('set relativenumber')
+	end})
+  use {
+    "folke/which-key.nvim",
     config = function()
-      require("conform").setup()
-    end,
-  })
+	    vim.o.timeout = true
+	    vim.o.timeoutlen = 300
+	    require("which-key").setup {
 
--- Lua
-use {
-  "folke/which-key.nvim",
-  config = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-    require("which-key").setup {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    }
-  end
-}
+		    -- your configuration comes here
+		    -- or leave it empty to use the default settings
+		    -- refer to the configuration section below
+            }
+    end}
+  use {'nvim-lualine/lualine.nvim',
+  requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
 end)
 
